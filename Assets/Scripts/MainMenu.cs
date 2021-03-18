85d177f6418eb53b7bad3d60173221101dd1b4e4
Sabina <sabbe1000@gmail.com>
2021-03-18 18:01:34 +0100

    Lose screen implemented. Gameplay almost done.

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void OpenMenu ()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 3);
+    }
+  
+}
